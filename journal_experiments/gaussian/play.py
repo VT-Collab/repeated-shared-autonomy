98c306772ff0c5f31f2f28328c1a7c8c9f7c370a
ajonnavittula <ajonnavittula@wpi.edu>
2022-01-10 16:09:19 -0500

    with 5 runs

--- journal_experiments/gaussian/play.py
+++ journal_experiments/gaussian/play.py
@@ -301,10 +301,10 @@ def main():
     conn = connect2robot(PORT)
     interface = Joystick()
     x = []
-    g_range = np.arange(0.3,0.7,0.001)
+    g_range = np.arange(0.3,0.7,0.002)
     for gx in g_range:
         final_x = []
-        for _ in range(1):
+        for _ in range(5):
             final_state = run(conn, interface, gx)
             poi = final_state[0]
             final_x.append(poi)
