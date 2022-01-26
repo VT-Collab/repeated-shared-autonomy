b8c90935cde5cea91ec72ee6d95e582d5d19db15
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2022-01-26 15:24:20 -0500

    reorganizing

--- journal_experiments/gaussian/1D/play.py
+++ journal_experiments/gaussian/1D/play.py
@@ -26,8 +26,8 @@ class Model(object):
         model_dict = torch.load(classifier_name, map_location='cpu')
         self.class_net.load_state_dict(model_dict)
         
-        # model_dict = torch.load(cae_name, map_location='cpu')
-        # self.cae_net.load_state_dict(model_dict)
+        model_dict = torch.load(cae_name, map_location='cpu')
+        self.cae_net.load_state_dict(model_dict)
 
         self.class_net.eval
         self.cae_net.eval
@@ -37,14 +37,14 @@ class Model(object):
         confidence = F.softmax(labels, dim=0)
         return confidence.data[0].numpy()
 
-    # def encoder(self, c):
-    #     z_mean_tensor = self.cae_net.encoder(torch.FloatTensor(c))
-    #     return z_mean_tensor.tolist()
+    def encoder(self, c):
+        z_mean_tensor = self.cae_net.encoder(torch.FloatTensor(c))
+        return z_mean_tensor.tolist()
 
-    # def decoder(self, z, s):
-    #     z_tensor = torch.FloatTensor(z + s)
-    #     a_predicted = self.cae_net.decoder(z_tensor)
-    #     return a_predicted.data.numpy()
+    def decoder(self, z, s):
+        z_tensor = torch.FloatTensor(z + s)
+        a_predicted = self.cae_net.decoder(z_tensor)
+        return a_predicted.data.numpy()
 
 
 def run(gstar, iter, vae):
@@ -61,24 +61,26 @@ def run(gstar, iter, vae):
 
     state = 0
     # goal = np.random.multivariate_normal(GOAL_D, SIGMA_D)
-    sigma_h = 1
-    sigma_d = 1
+    sigma_h = .1
+    sigma_d = .1
     a_h = np.random.normal(gstar-state, sigma_h)
-    a_r = np.random.normal(5-state, sigma_d)
+    # a_r = np.random.normal(.5-state, sigma_d)
+    z = model.encoder([state, 1.0])
+    a_r = model.decoder(z, [state, 1.0])
     alpha = model.classify([state, a_h])
-    # alpha = min(alpha, 0.85)
+    alpha = min(alpha, 0.85)
     # print("a_h:{} alpha:{}".format(a_h, alpha))
     return [state + (1 - alpha) * a_h + alpha * a_r, alpha]
 
 
 def main():
     x = []
-    g_range = np.arange(0,10.1,0.1)
+    g_range = np.arange(0,1.01,0.01)
     vae = "novae"
     avg_state = []
     for gx in g_range:
         final_state = []
-        for iter in range(100000):
+        for iter in range(1000):
             res = run(gx, iter, vae)
             final_state.append(res[0])
         avg_state.append(np.mean(final_state))
@@ -90,7 +92,7 @@ def main():
     # pickle.dump([g_range, x], open("final_state.pkl", "wb"))
     err = g_range - avg_state
     err = err.tolist()
-    pickle.dump(err, open("err_vs_gstar.pkl", "wb"))
+    pickle.dump(err, open("err_vs_gstar_vae.pkl", "wb"))
     plt.plot(g_range.tolist(), err)
     plt.show()
 
--- journal_experiments/gaussian/1D/train_cae.py
+++ journal_experiments/gaussian/1D/train_cae.py
@@ -25,11 +25,9 @@ class MotionData(Dataset):
 
     def __getitem__(self, idx):
         item = self.data[idx]
-        snippet = torch.FloatTensor(item[0]).to(device)
-        state = torch.FloatTensor(item[1]).to(device)
-        true_z = torch.FloatTensor(item[2]).to(device)
-        action = torch.FloatTensor(item[3]).to(device)
-        return (snippet, state, true_z, action)
+        state = torch.as_tensor([item[0], 1]).to(device)
+        action = torch.as_tensor(item[1]).to(device)
+        return (state, action)
 
 
 # conditional autoencoder
@@ -42,20 +40,20 @@ class CAE(nn.Module):
 
         # Encoder
         self.enc = nn.Sequential(
-            nn.Linear(6, 10),
-            nn.Tanh(),
+            # nn.Linear(2, 5),
+            # nn.Tanh(),
             # nn.Linear(15, 10),
             # nn.Tanh(),
-            nn.Linear(10, 1)
+            nn.Linear(2, 1)
         )
 
         # Policy
         self.dec = nn.Sequential(
-            nn.Linear(4, 20),
-            nn.Tanh(),
-            nn.Linear(20, 15),
-            nn.Tanh(),
-            nn.Linear(15, 3)
+            # nn.Linear(3, 3),
+            # nn.Tanh(),
+            # nn.Linear(5, 5),
+            # nn.Tanh(),
+            nn.Linear(3, 1)
         )
 
     def encoder(self, x):
@@ -65,10 +63,10 @@ class CAE(nn.Module):
         return self.dec(z_with_state)
 
     def forward(self, x):
-        history, state, ztrue, action = x
-        z = self.encoder(history)
-        ztrue = torch.FloatTensor(ztrue).to(device)
+        state, action = x
+        z = self.encoder(state)
         z_with_state = torch.cat((z, state), 1)
+        print(state)
         action_decoded = self.decoder(z_with_state)
         loss = self.loss(action, action_decoded)
         return loss
@@ -76,56 +74,17 @@ class CAE(nn.Module):
     def loss(self, action_decoded, action_target):
         return self.loss_func(action_decoded, action_target)
 
-def joint2pose(q):
-    def RotX(q):
-        return np.array([[1, 0, 0, 0], [0, np.cos(q), -np.sin(q), 0], [0, np.sin(q), np.cos(q), 0], [0, 0, 0, 1]])
-    def RotZ(q):
-        return np.array([[np.cos(q), -np.sin(q), 0, 0], [np.sin(q), np.cos(q), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-    def TransX(q, x, y, z):
-        return np.array([[1, 0, 0, x], [0, np.cos(q), -np.sin(q), y], [0, np.sin(q), np.cos(q), z], [0, 0, 0, 1]])
-    def TransZ(q, x, y, z):
-        return np.array([[np.cos(q), -np.sin(q), 0, x], [np.sin(q), np.cos(q), 0, y], [0, 0, 1, z], [0, 0, 0, 1]])
-    H1 = TransZ(q[0], 0, 0, 0.333)
-    H2 = np.dot(RotX(-np.pi/2), RotZ(q[1]))
-    H3 = np.dot(TransX(np.pi/2, 0, -0.316, 0), RotZ(q[2]))
-    H4 = np.dot(TransX(np.pi/2, 0.0825, 0, 0), RotZ(q[3]))
-    H5 = np.dot(TransX(-np.pi/2, -0.0825, 0.384, 0), RotZ(q[4]))
-    H6 = np.dot(RotX(np.pi/2), RotZ(q[5]))
-    H7 = np.dot(TransX(np.pi/2, 0.088, 0, 0), RotZ(q[6]))
-    H_panda_hand = TransZ(-np.pi/4, 0, 0, 0.2105)
-    H = np.linalg.multi_dot([H1, H2, H3, H4, H5, H6, H7, H_panda_hand])
-    return H[:,3][:3]
-
-
 # train cAE
 def main():
     tasks = int(sys.argv[1])
 
     dataset = []
     folder = 'demos'
-    lookahead = 5
-    noiselevel = 0.05
-    noisesamples = 5
 
-    notepad_count = 0
-    tape_count = 0
-    glass_count = 0
-    shelf_count = 0
-    cup_count = 0
     savename = 'data/' + '0_cae_' + str(tasks) + '.pkl'
     for filename in os.listdir(folder):
-        demo = pickle.load(open(folder + "/" + filename, "rb"))
-        traj = [item[0] for item in demo]
-        # print(traj)
-        n_states = len(traj)
-        z = [1.0]
-        for idx in range(n_states-lookahead):
-            home_state = traj[idx][:3]
-            position = traj[idx][3:]
-            nextposition = traj[idx + lookahead][3:]
-            for jdx in range(noisesamples):
-                action = 5 * (nextposition - (position + np.random.normal(0, noiselevel, 3)))
-                dataset.append((home_state + position, position, z, action.tolist()))
+        if filename[0] != ".":
+            dataset = pickle.load(open(folder + "/" + filename, "rb"))
 
     pickle.dump(dataset, open(savename, "wb"))
     print(dataset[0])
@@ -155,7 +114,7 @@ def main():
             loss.backward()
             optimizer.step()
         scheduler.step()
-        print(epoch, loss.item())
+        # print(epoch, loss.item())
         torch.save(model.state_dict(), savename)
 
 
