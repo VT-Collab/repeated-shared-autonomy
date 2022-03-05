2aa66cf2ff9dd8826b3c6a2244484f271b845f65
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2022-03-04 22:21:29 -0500

    working on deformations

--- simulations/ur_10/scratchwork.py
+++ simulations/ur_10/scratchwork.py
@@ -4,11 +4,14 @@ import torch.optim as optim
 from torch.utils.data import Dataset, DataLoader
 import pickle
 import random
-import numpy as np
 from sklearn.utils import shuffle
 import sys
 import os
 import copy 
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = "cpu"
 # Clear GPU memory from previous runs
@@ -16,26 +19,58 @@ if device == "cuda":
     torch.cuda.empty_cache()
 
 data1 = pickle.load(open("demos/push1_1.pkl","rb"))
-data2 = pickle.load(open("demos/push1_2.pkl","rb"))
-latent_dim = 5
-#RNN
-gru = nn.GRU(
-    input_size = 18,
-    hidden_size = latent_dim,
-    num_layers = 1,
-    batch_first = True
-    )
-fcn = nn.Sequential(
-    nn.Linear(latent_dim, 5),
-    nn.Tanh(),
-    nn.Linear(5, 2)
-)
-traj = [item[0] + item[1] for item in data1]
 
-i1 = torch.Tensor(traj).unsqueeze(0)
+# plotting deformations
+def deform(xi, start, length, tau):
+    # length += np.random.choice(np.arange(50, 250))
+    xi1 = copy.deepcopy(np.asarray(xi))
+    A = np.zeros((length+2, length))
+    for idx in range(length):
+        A[idx, idx] = 1
+        A[idx+1,idx] = -2
+        A[idx+2,idx] = 1
+    R = np.linalg.inv(np.dot(A.T, A))
+    U = np.zeros(length)
+    gamma = np.zeros((length, 3))
+    for idx in range(3):
+        U[0] = tau[idx]
+        gamma[:,idx] = np.dot(R, U)
+    end = min([start+length, xi1.shape[0]-1])
+    xi1[start:end,:] += gamma[0:end-start,:]
+    return xi1
+traj = np.array([item[0] for item in data1])
+    
+fig = plt.figure()
+ax = fig.gca(projection='3d')    
+ax.plot(traj[:,6], traj[:,7], traj[:,8], label='parametric curve')
+tau = np.random.uniform([-0.04]*6, [0.04]*6)
+traj[:,6:9] = deform(traj[:,6:9], 0, len(traj), tau) 
+ax.plot(traj[:,6], traj[:,7], traj[:,8])#, label='parametric curve')
+plt.show()
+
+
+
+# data1 = pickle.load(open("demos/push1_1.pkl","rb"))
+# data2 = pickle.load(open("demos/push1_2.pkl","rb"))
+# latent_dim = 5
+#RNN
+# gru = nn.GRU(
+#     input_size = 18,
+#     hidden_size = latent_dim,
+#     num_layers = 1,
+#     batch_first = True
+#     )
+# fcn = nn.Sequential(
+#     nn.Linear(latent_dim, 5),
+#     nn.Tanh(),
+#     nn.Linear(5, 2)
+# )
+# traj = [item[0] + item[1] for item in data1]
+#
+# i1 = torch.Tensor(traj).unsqueeze(0)
 # i1 = i1.unsqueeze(0)
-target = np.zeros(len(traj))
-print(target.shape)
+# target = np.zeros(len(traj))
+# print(target.shape)
 # traj = [item[0] + item[1] for item in data2]
 # i2 = torch.Tensor(traj)
 # i2 = i2.unsqueeze(0)
