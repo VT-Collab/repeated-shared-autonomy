6bf26963154afe3f844f56e48bd24e11067bea6f
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2022-03-14 04:44:42 -0400

    trained individual models and collected data for regret computation

--- simulations/ur_10/scheduler.sh
+++ simulations/ur_10/scheduler.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# python train_classifier_old.py &
-# python train_cae_old.py &
-# wait
-# python collect_data.py
-# wait
+python train_classifier_old.py &
+python train_cae_old.py &
+wait
+python collect_data.py
+wait
 find * -size -4M -type f -print0 | xargs -0 git add
 wait
-git commit -m "testing bash scheduling"
+git commit -m "trained individual models and collected data for regret computation"
 wait 
 git push origin main
