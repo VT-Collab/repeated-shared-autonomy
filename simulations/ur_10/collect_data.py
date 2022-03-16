c36412bfae2336ec1e0baf5e24ea43b477576ad5
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2022-03-16 00:45:54 -0400

    ran models with 8 latent z

--- simulations/ur_10/collect_data.py
+++ simulations/ur_10/collect_data.py
@@ -69,7 +69,7 @@ def main(model_name, max_runs):
     joystick = JoystickControl()
 
     cae_model = 'models/latent_z_8/' + 'cae_' + "_".join(model_name) + "_old"
-    class_model = 'models/latent_z_8/' + 'class_' + "_".join(model_name) + "_old"
+    class_model = 'models/' + 'class_' + "_".join(model_name) + "_old"
     model = Model(class_model, cae_model)
     
     while not rospy.is_shutdown():
@@ -133,8 +133,8 @@ def main(model_name, max_runs):
 
                     if assist:
                         # alpha = .8
-                        # qdot = (alpha * 2.5 * np.asarray(qdot_r) + (1-alpha) * np.asarray(qdot_h))#*2.0
-                        qdot = (0.8 * 2.5 * np.asarray(qdot_r) + 0.2 * np.asarray(qdot_h))#*2.0
+                        qdot = (alpha * 2.5 * np.asarray(qdot_r) + (1-alpha) * np.asarray(qdot_h))#*2.0
+                        # qdot = (0.8 * 2.5 * np.asarray(qdot_r) + 0.2 * np.asarray(qdot_h))#*2.0
                         qdot = np.clip(qdot, -0.1, 0.1)
                         qdot = qdot.tolist()[0]
                     else:
--- simulations/ur_10/scheduler.sh
+++ simulations/ur_10/scheduler.sh
@@ -4,6 +4,6 @@ python collect_data.py
 wait
 find * -size -4M -type f -print0 | xargs -0 git add
 wait
-git commit -m "trained individual models and collected data for regret computation"
+git commit -m "ran models with 8 latent z"
 wait 
 git push origin main
