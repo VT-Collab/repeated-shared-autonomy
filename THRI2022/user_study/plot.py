4496387295f1c746d5444ee6e2d341b720fa871e
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2023-01-16 12:32:58 -0500

    some light cleanup

--- THRI2022/user_study/plot.py
+++ THRI2022/user_study/plot.py
@@ -5,14 +5,17 @@ import glob
 
 # plot for task 1
 # Traverse all users, find task1 folder, collect all files
-# folder = "./user_data/"
-# ignored_folders = ["user0", "pilot"]
-# user_folders = glob.glob(folder + "user*")
+folder = "./user_data/"
+ignored_folders = ["user0", "pilot"]
+user_folders = glob.glob(folder + "user*")
 
-# for user in user_folders:
-#     task_no = "Task1"
-#     task1_files = glob.glob(user + "/" + task_no + "/*")
-#     for file in task1_files:
-#         data = pickle.load(file)
-d = pickle.load(open("./user_data/user1/Task1/casa_lemon_2_0.pkl", "rb"))
-print(d)
+for user in user_folders:
+    task_no = "Task1"
+    task1_files = glob.glob(user + "/" + task_no + "/*")
+    for file in task1_files:
+        try:
+            data = pickle.load(open(file, "rb"))
+        except:
+            print(file)
+# d = pickle.load(open("./user_data/user1/Task1/casa_lemon_2_0.pkl", "rb"))
+# print(d)
